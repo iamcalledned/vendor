ce68d483ad917e42e0954322d23f6d6803570dc9
ned <iamcalledned@gmail.com>
2012-11-14 15:53:55 -0500

    adding first set of proprietary files.  SuperUser and 4.2 market

---
+++ ned/products/common.mk
@@ -0,0 +1,26 @@
+# Generic product
+PRODUCT_NAME := ned
+PRODUCT_BRAND := ned
+PRODUCT_DEVICE := generic
+
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/ned/overlay/common
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.feedback \
+    ro.setupwizard.enterprise_mode=1
+
+# Blobs common to all devices
+PRODUCT_COPY_FILES += \
+    vendor/ned/proprietary/common/app/Phonesky.apk:system/app/Phonesky.apk \
+    vendor/ned/proprietary/common/app/Superuser.apk:system/app/Superuser.apk \
+    vendor/ned/proprietary/common/xbin/su:system/xbin/su
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
