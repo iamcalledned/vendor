c16c5005db5abf31428ece84e5c6dea841c5fe23
ned <iamcalledned@gmail.com>
2012-12-05 14:49:05 -0500

    changed defaults around to see if I could get the right stuff to build; c

--- ned/products/manta.mk
+++ ned/products/manta.mk
@@ -6,18 +6,21 @@ $(call inherit-product, vendor/ned/proprietary/manta/Android.mk)
 
 # Setup device specific product configuration.
 PRODUCT_NAME := ned_manta
-PRODUCT_BRAND := google
+PRODUCT_BRAND := googlened
 PRODUCT_DEVICE := manta
 PRODUCT_MODEL := Nexus 10
 PRODUCT_MANUFACTURER := samsung
+BUILD_FINGERPRINT := google/mantaray/manta:4.2.1/JOP40D/533553:user/release-keys
+PRIVATE_BUILD_DESC := mantaray-user 4.2.1 JOP40D 533553 release-keys
+BUILD_NUMBER := 533553
+BUILD_ID := JOP40D
 
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mantaray-user BUILD_FINGERPRINT=google/mantaray/manta:4.2/JOP40C/527662:user/release-keys PRIVATE_BUILD_DESC="mantaray-user 4.2 JOP40C 527662 release-keys" BUILD_NUMBER=527662
 
-PRODUCT_PACKAGES += \
-        SetupWizard
 
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.setupwizard.enterprise_mode=1
+
+
 
 
 
